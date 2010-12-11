db26c1d256171a54b30a23844c9a96a21742e969
Markus Törnqvist <mjt@fadconsulting.com>
2010-12-11 20:32:23 +0200

    Include URLs

--- django_mises/urls.py
+++ django_mises/urls.py
@@ -7,8 +7,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^django_mises/', include('django_mises.foo.urls')),
+    (r'^', include('django_mises.main.urls')),
+    (r'^', include('django_mises.blog.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
