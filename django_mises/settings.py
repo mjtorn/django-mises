7efe6d3582af59122bcbfa7dcd2ca3ec0e020210
Markus Törnqvist <mjt@fadconsulting.com>
2010-12-11 20:31:48 +0200

    Install main application

--- django_mises/settings.py
+++ django_mises/settings.py
@@ -95,6 +95,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django_mises.main',
     'django_mises.blog'
 )
 
