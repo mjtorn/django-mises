2297c6d928449be960851591e0823acc042d2e42
Markus Törnqvist <mjt@fadconsulting.com>
2010-12-11 21:39:35 +0200

    Blog post view

--- django_mises/blog/views.py
+++ django_mises/blog/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from django.template import RequestContext
+
+from django.shortcuts import get_object_or_404, render_to_response
+
+from blog import models as blog_models
+
+import datetime
+
+def index(request, post_id, slug):
+    """Index view
+    """
+
+    now = datetime.datetime.now()
+
+    post = get_object_or_404(blog_models.Post, id=post_id, publish_at__lte=now)
+
+    context = {
+        'post': post,
+        'title': post.title,
+    }
+    req_ctx = RequestContext(request, context)
+
+    return render_to_response('post.html', req_ctx)
+
+# EOF
+
+
