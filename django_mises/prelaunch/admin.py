506f5d42899dd555e0a1399cd50a03c50aa55f1d
Markus Törnqvist <mjt@fadconsulting.com>
2011-01-13 01:13:30 +0200

    Invitation model with admin

---
+++ django_mises/prelaunch/admin.py
@@ -0,0 +1,42 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from django.contrib.auth import models as auth_models
+
+from django.utils.translation import ugettext_lazy as _
+
+from django.contrib import admin
+
+from django.db import models
+
+from django_mises.prelaunch import models as mises_models
+
+from django import forms
+
+class InvitationAdminForm(forms.ModelForm):
+    class Meta:
+        model = mises_models.Invitation
+
+    def clean_email(self):
+        email = self.data['email']
+        try:
+            # Because django authenticates by username, username is email
+            auth_models.User.objects.get(username=email)
+            raise forms.ValidationError(_('Found user for email'))
+        except auth_models.User.DoesNotExist:
+            return email
+
+
+class InvitationAdmin(admin.ModelAdmin):
+    form = InvitationAdminForm
+
+    def get_readonly_fields(self, request, obj=None):
+        """What are we not allowed to edit
+        """
+
+        return ('secret', 'created_at', 'used_at')
+
+
+admin.site.register(mises_models.Invitation, InvitationAdmin)
+
+# EOF
+
---
+++ django_mises/prelaunch/models.py
@@ -0,0 +1,77 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from django.contrib.auth import models as auth_models
+
+from django.utils.translation import ugettext_lazy as _
+
+from django.db import models
+
+import hashlib
+
+import random
+
+import time
+
+## The immutable principle on which all speculation is impossible
+AUTHOR_GROUP = auth_models.Group.objects.get(name='Authors')
+
+class Invitation(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=75, unique=True)
+
+    secret = models.CharField(max_length=40)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    used_at = models.DateTimeField(null=True, blank=True, default=None)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            from django.core import mail
+
+            secret = '%s%s' % (str(random.random()).split('.')[1], str(time.time()).replace('.', ''))
+            self.secret = hashlib.sha1(secret).hexdigest()
+
+            super(Invitation, self).save(*args, **kwargs)
+
+            user = auth_models.User()
+
+            user.username = user.email = self.email
+            user.first_name = self.first_name
+            user.last_name = self.last_name
+
+            # To access admin
+            user.is_staff = True
+
+            # But not yet
+            user.is_active = False
+
+            user.save()
+
+            user.groups.add(AUTHOR_GROUP)
+
+            email = mail.EmailMessage(
+                to = (user.email,),
+                subject = _('Invitation to mises.fi'),
+                body = """\
+Hei!
+
+Teidät on kutsuttu Suomen Ludwig von Mises -Instituutin sivuille.
+
+Ystävällisesti klikatkaa linkkiä http://mises.fi/admin/invitation/%s asettaaksenne salasanan.
+Käyttäjätunnus on sama kuin sähköpostinne.
+
+Tervetuloa!
+
+-- 
+mises
+
+                """ % self.secret
+            )
+
+            email.send()
+        else:
+            super(Invitation, self).save(*args, **kwargs)
+
+# EOF
+
