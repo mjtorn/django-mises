0a850dbf76b531dc711712b8eb7ee10d19d7b12e
Markus Törnqvist <mjt@fadconsulting.com>
2011-01-16 00:55:31 +0200

    Form for inputting username

--- django_mises/prelaunch/forms.py
+++ django_mises/prelaunch/forms.py
@@ -3,6 +3,7 @@
 from django.utils.translation import ugettext_lazy as _
 
 from django.contrib.auth import forms as auth_forms
+from django.contrib.auth import models as auth_models
 
 from django import forms
 
@@ -20,6 +21,25 @@ class AuthenticateAdminForm(auth_forms.AuthenticationForm):
             raise forms.ValidationError(_('User is not admin'))
 
         return self.cleaned_data
+
+
+class UsernameForm(forms.Form):
+    """Prompt for username
+    """
+
+    ## Copypasta courtesy of django
+    username = forms.RegexField(label=_("Username"), max_length=30, regex=r'^[\w.@+-]+$',
+        help_text = _("Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only."),
+        error_messages = {'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            auth_models.User.objects.get(username=username)
+        except auth_models.User.DoesNotExist:
+            return username
+        raise forms.ValidationError(_("A user with that username already exists."))
+
 
 # EOF
 
