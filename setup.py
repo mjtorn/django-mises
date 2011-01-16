7f3807bd78a22f3135f8eaad7735553d562baacd
Markus Törnqvist <mjt+mises@nysv.org>
2011-01-16 17:19:32 +0000

    setup and MANIFEST

---
+++ setup.py
@@ -0,0 +1,63 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from setuptools import setup
+
+import os
+import sys
+
+NAME = 'django_mises'
+AUTHOR_EMAIL = 'mjt@nysv.org'
+URL = 'http://django_mises.fi/'
+
+def get_version():
+    stdin_f, stdout_f, stderr_f = os.popen3('git ls-remote .')
+
+    stderr = stderr_f.read()
+    if stderr:
+        print stderr
+        sys.exit(1)
+
+    stdout = stdout_f.readlines()
+
+    head = None
+    tag = None
+    for line in stdout:
+        hash, name = line.split()
+        if name == 'HEAD':
+            head = hash
+
+        if head and hash == head and 'tag' in name:
+            tag = name.rsplit('/', 1)[-1]
+            tag = '.'.join(tag.split('.')[:-1])
+
+    if tag is None:
+        print 'tag not found'
+        sys.exit(1)
+
+    return tag
+
+packages = []
+
+def get_packages(arg, dir, fnames):
+    global packages
+
+    if '__init__.py' in fnames:
+        packages.append(dir.replace('/', '.'))
+
+os.path.walk(NAME, get_packages, None)
+
+setup(
+    name = NAME,
+    version = get_version(),
+    author = 'Markus Törnqvist',
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    packages = packages,
+#    package_data = package_data, # MANIFEST.in where available
+    include_package_data = True,
+    long_description = '%s.' % NAME,
+    install_requires = ('django==1.2.3', 'PIL', 'django-annoying==0.7.5', 'django-ckeditor==0.0.8', 'pyquery==0.6.1', 'lxml==2.3beta1'),
+)
+
+# EOF
+
