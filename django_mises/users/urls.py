31d3c616892f8932d2b5f38c81b2258658bc9b09
Markus Törnqvist <mjt@fadconsulting.com>
2011-01-16 14:11:43 +0200

    Logout url

--- django_mises/users/urls.py
+++ django_mises/users/urls.py
@@ -1,5 +1,7 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
+from django.contrib.auth import views as auth_views
+
 from django.contrib.flatpages import views as flatpages_views
 
 from django.conf.urls.defaults import *
@@ -9,6 +11,9 @@ from users import views
 
 urlpatterns = patterns('',
     url(r'^user/(?P<username>[\w.@+-]+)$', views.user_view, name='user'),
+
+    ## Plagiate this stuff from django
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
 )
 
 # EOF
