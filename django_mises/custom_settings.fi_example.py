e4e3dc875581fcf231fab6235e01c0283f98cd92
Markus Törnqvist <mjt@fadconsulting.com>
2011-01-16 17:41:03 +0200

    custom_settings example for Finnish site

---
+++ django_mises/custom_settings.fi_example.py
@@ -0,0 +1,25 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+import settings
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+settings.DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+settings.DATABASES['default']['NAME'] = '/home/mjt/src/git_checkouts/django-mises/mises.db'
+
+MEDIA_ROOT = '/home/mjt/src/git_checkouts/django-mises/django_mises/media/'
+
+CKEDITOR_UPLOAD_PATH = '%supload/' % MEDIA_ROOT
+
+TIME_ZONE = 'Europe/Helsinki'
+
+USE_L10N = True
+LANGUAGE_CODE = 'fi'
+
+DATE_FORMAT = 'l j.m.Y.'
+TIME_FORMAT = 'h:i'
+DATETIME_FORMAT = 'j.m.Y. h:i'
+
+# EOF
+
