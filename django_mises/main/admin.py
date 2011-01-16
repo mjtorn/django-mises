a4f7e9a41a4c3e92ec60025368b6670768e12914
Markus Törnqvist <mjt@fadconsulting.com>
2011-01-16 15:34:16 +0200

    Register slogan model into admin

---
+++ django_mises/main/admin.py
@@ -0,0 +1,10 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from django.contrib import admin
+
+from django_mises.main import models
+
+admin.site.register(models.Slogan)
+
+# EOF
+
