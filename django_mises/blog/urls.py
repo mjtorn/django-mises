0b0512d341808d7f632497963418c1a12f957281
Markus Törnqvist <mjt+mises@nysv.org>
2011-03-16 23:03:41 +0000

    Reorder post urls

--- django_mises/blog/urls.py
+++ django_mises/blog/urls.py
@@ -6,9 +6,9 @@ from django.conf import settings
 from blog import views as blog_views
 
 urlpatterns = patterns('',
-    url(r'^post/(?P<post_id>\d+)/(?P<slug>.+)/preview$', blog_views.post, {'preview': True}, name='preview'),
-    url(r'^post/(?P<post_id>\d+)/(?P<slug>.+)$', blog_views.post, name='post'),
     url(r'^post/(?P<post_id>\d+)$', blog_views.post, name='post'),
+    url(r'^post/(?P<post_id>\d+)/(?P<slug>.+)$', blog_views.post, name='post'),
+    url(r'^post/(?P<post_id>\d+)/(?P<slug>.+)/preview$', blog_views.post, {'preview': True}, name='preview'),
 )
 
 # EOF
