5701d307c81991bfd1df5b756a18fc24428742b3
Markus Törnqvist <mjt@fadconsulting.com>
2011-04-19 23:23:11 +0300

    Publish RSS

--- django_mises/blog/urls.py
+++ django_mises/blog/urls.py
@@ -4,11 +4,16 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 from blog import views as blog_views
+from blog import feeds as blog_feeds
 
 urlpatterns = patterns('',
     url(r'^post/(?P<post_id>\d+)/(?P<slug>.+)/preview$', blog_views.post, {'preview': True}, name='preview'),
     url(r'^post/(?P<post_id>\d+)/(?P<slug>.+)$', blog_views.post, name='post'),
     url(r'^post/(?P<post_id>\d+)$', blog_views.post, name='post'),
+
+    url(r'^rss.xml$', blog_feeds.ArticlesFeed()),
+    # compatibility
+    url(r'^dailyarticles.xml$', blog_feeds.ArticlesFeed()),
 )
 
 # EOF
