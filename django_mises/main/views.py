579c833828ec6809d69bb90bfc13b33aaf352b50
Markus Törnqvist <mjt@fadconsulting.com>
2011-12-27 10:29:18 +0200

    404 and 500 views, with template

--- django_mises/main/views.py
+++ django_mises/main/views.py
@@ -15,5 +15,28 @@ def index(request):
 
     return render_to_response('index.html', req_ctx)
 
+from django.conf import settings
+
+def handler404(request):
+    """General 404
+    """
+
+    context = {
+        
+    }
+    req_ctx = RequestContext(request, context)
+
+    return render_to_response('404.html', req_ctx)
+
+from django.template import Context, loader
+
+def handler500(request, template_name='500.html'):
+    """http://djangosnippets.org/snippets/1199/
+    """
+
+    from django.http import HttpResponseServerError
+    t = loader.get_template(template_name)
+    return HttpResponseServerError(t.render(Context({'MEDIA_URL': settings.MEDIA_URL})))
+
 # EOF
 
